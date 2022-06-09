015529f6ea0f5d667fdb8e26b691eff204bb3299
A601-11 <tnk.dat.class@gmail.com>
2022-06-09 13:40:46 +0900

    Initial commit

---
+++ fukuv0609/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuv0609
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("杉田夏葵");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = $"{int.Parse(textBox1.Text) + int.Parse(textBox2.Text)}";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
